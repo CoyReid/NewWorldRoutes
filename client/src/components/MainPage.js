a9d81d0d0f06ab0def9201c1d026491daf57a87a
Coy Reid <coyreid7@gmail.com>
2021-10-25 15:30:04 -0500

    got search and filters working on home page

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -8,17 +8,21 @@ import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
 import { NavLink } from "react-router-dom";
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import SearchSortBar from "./SearchSortBar";
 
 const columns = [
   { id: "region", label: "Route Region", minWidth: 170 },
   { id: "routeresource", label: "Route Resource", minWidth: 100 },
 ];
 
-export default function MainPage({routes}) {
+export default function MainPage({ routes }) {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [search, setSearch] = useState("");
+  const [firstFilter, setFirstFilter] = useState("all");
+  const [secondFilter, setSecondFilter] = useState("all");
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -29,69 +33,135 @@ export default function MainPage({routes}) {
     setPage(0);
   };
 
+  const searchedFilteredRoutes = routes
+    .filter(
+      (route) =>
+        route.region.toLowerCase().includes(search.toLowerCase()) ||
+        route.routeresource.toLowerCase().includes(search.toLowerCase())
+    )
+    .filter((route) => {
+      if (firstFilter === "all" || route.region.toLowerCase() === firstFilter) {
+        return true;
+      } else {
+        return false;
+      }
+    })
+    .filter((route) => {
+      if (
+        secondFilter === "all" ||
+        route.routeresource.toLowerCase() === secondFilter
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
   return (
     <Grid container spacing={2} className="mainPageGrid">
-    <Grid item xs={12}>
-    <Typography variant="h2" component="div" className="mainPageText">
-        Welcome to New World Routes!
-      </Typography>
-    <Typography variant="body1" component="div" className="mainPageText" id="mainPageBodyTextOne">
-        New World is a thrilling, open-world MMO filled with danger and opportunity where you'll forge a new destiny for yourself as an adventurer shipwrecked on the supernatural island of Aeternum.
-      </Typography>
-      <Typography variant="body2" component="div" className="mainPageText" id="mainPageBodyTextTwo">
-        New World comes with a variety of fun, interactive features including gathering/crafting. One of the primary gathering skills is Mining, which requires a pickaxe and allows you to mine ores/resources in each region.
-        As mining is tedious and time consuming process, this website is meant to help players generate and save routes for mining ores in an efficient manner. 
-      </Typography>
-      <Typography variant="body2" component="div" className="mainPageText" id="mainPageBodyTextThree">
-        Click on a route in the table below to go to it's page!
-      </Typography>
-    </Grid>
-    <Paper sx={{ width: "50%", overflow: "hidden" }} className="mainTable">
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead >
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {routes
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((route) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={route.id}>
-                    {columns.map((column) => {
-                      const value = route[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align} component={NavLink} to={`/routes/${route.id}`} style={{ textDecoration: 'none' }}>
-                          {value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={routes.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
+      <Grid item xs={12}>
+        <Typography variant="h2" component="div" className="mainPageText">
+          Welcome to New World Routes!
+        </Typography>
+        <Typography
+          variant="body1"
+          component="div"
+          className="mainPageText"
+          id="mainPageBodyTextOne"
+        >
+          New World is a thrilling, open-world MMO filled with danger and
+          opportunity where you'll forge a new destiny for yourself as an
+          adventurer shipwrecked on the supernatural island of Aeternum.
+        </Typography>
+        <Typography
+          variant="body2"
+          component="div"
+          className="mainPageText"
+          id="mainPageBodyTextTwo"
+        >
+          New World comes with a variety of fun, interactive features including
+          gathering/crafting. One of the primary gathering skills is Mining,
+          which requires a pickaxe and allows you to mine ores/resources in each
+          region. As mining is tedious and time consuming process, this website
+          is meant to help players generate and save routes for mining ores in
+          an efficient manner.
+        </Typography>
+        <Typography
+          variant="body2"
+          component="div"
+          className="mainPageText"
+          id="mainPageBodyTextThree"
+        >
+          Click on a route in the table below to go to it's page!
+        </Typography>
+      </Grid>
+      <Grid item xs={7}>
+        <SearchSortBar
+          search={search}
+          onSearchChange={setSearch}
+          firstFilter={firstFilter}
+          onFirstFilterChange={setFirstFilter}
+          secondFilter={secondFilter}
+          onSecondFilterChange={setSecondFilter}
+        />
+      </Grid>
+      <Paper sx={{ width: "50%", overflow: "hidden" }} className="mainTable">
+        <TableContainer sx={{ maxHeight: 440 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
+                  >
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {searchedFilteredRoutes
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((route) => {
+                  return (
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={route.id}
+                    >
+                      {columns.map((column) => {
+                        const value = route[column.id];
+                        return (
+                          <TableCell
+                            key={column.id}
+                            align={column.align}
+                            component={NavLink}
+                            to={`/routes/${route.id}`}
+                            style={{ textDecoration: "none" }}
+                          >
+                            {value}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={searchedFilteredRoutes.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
     </Grid>
   );
 }
--- client/src/components/SearchSortBar.js
+++ client/src/components/SearchSortBar.js
@@ -1,5 +1,69 @@
-export default function SearchSortBar(){
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { TextField } from '@mui/material';
+
+export default function SearchSortBar({search, onSearchChange, firstFilter, onFirstFilterChange, secondFilter, onSecondFilterChange}){
+
+  function handleSearchChange(e) {
+    onSearchChange(e.target.value);
+  }
+
+  function handleFirstFilterChange(e) {
+    onFirstFilterChange(e.target.value);
+  }
+
+  function handleSecondFilterChange(e) {
+    onSecondFilterChange(e.target.value);
+  }
+
+  const regions = ["All", "First Light", "Cutlass Keys", "Windsward", "Monarch's Bluffs", "Reekwater", "Everfall", "Ebonscale Reach", "Brightwood", "Weaver's Fen", "Restless Shore", "Great Cleave", "Edengrove", "Mourningdale", "Shattered Mountain"];
+  const resources = ["All", "Gold", "Iron", "Saltpeter", "SeepingStone", "Silver", "Starmetal", "Lodestone", "Orichalcum", "Platinum"];
   return (
-    <div>this is SearchSortBar</div>
+    <div className="searchBar">
+      <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
+      <TextField
+        className="searchField"
+        id="outlined-secondary"
+        label="Search..."
+        variant="filled"
+        value={search}
+        onChange={handleSearchChange}
+      />
+      </FormControl>
+      <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
+      <InputLabel id="demo-simple-select-standard-label">Region</InputLabel>
+      <Select
+        className="filter"
+        labelId="demo-customized-select-label"
+        id="demo-customized-select"
+        onChange={handleFirstFilterChange}
+        value={firstFilter}
+      >
+        {regions.map((region) => (
+          <MenuItem value={region.toLowerCase()} key={region}>
+            {region}
+          </MenuItem>
+        ))}
+      </Select>
+      </FormControl>
+      <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
+      <InputLabel id="demo-simple-select-standard-label">Resource</InputLabel>
+      <Select
+        className="filter"
+        labelId="demo-customized-select-label"
+        id="demo-customized-select"
+        onChange={handleSecondFilterChange}
+        value={secondFilter}
+      >
+        {resources.map((resource) => (
+          <MenuItem value={resource.toLowerCase()} key={resource}>
+            {resource}
+          </MenuItem>
+        ))}
+      </Select>
+      </FormControl>
+    </div>
   )
 }
